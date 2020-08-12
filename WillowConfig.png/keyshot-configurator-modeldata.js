fe28b8e3cf702230eef93bd2030fa5770e08c6ff
JordanLuxion <69603295+JordanLuxion@users.noreply.github.com>
2020-08-12 13:13:15 -0700

    Add files via upload
    XR & Config

---
+++ WillowConfig.png/keyshot-configurator-modeldata.js
@@ -0,0 +1,76 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "tan%20%2f%20bronze.png",
+                    "multimaterials": [
+                        "4+1",
+                        "8+5",
+                        "24+20",
+                        "12+9",
+                        "17+13",
+                        "321+322"
+                    ],
+                    "name": "Tan / Bronze"
+                },
+                {
+                    "icon": "black.png",
+                    "multimaterials": [
+                        "4+2",
+                        "8+6",
+                        "24+21",
+                        "12+10",
+                        "17+15",
+                        "321+323"
+                    ],
+                    "name": "Black"
+                },
+                {
+                    "checked": true,
+                    "icon": "blues.png",
+                    "multimaterials": [
+                        "4+3",
+                        "8+7",
+                        "24+23",
+                        "12+11",
+                        "17+16",
+                        "321+324"
+                    ],
+                    "name": "Blues"
+                }
+            ],
+            "name": "Color Options"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "full%20set.png",
+            "materialgroups": [
+                "Color Options"
+            ],
+            "modelset": 550921240,
+            "name": "Full Set"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "front%20-%20white.png",
+            "id": 1572110392,
+            "name": "Front - White"
+        },
+        {
+            "icon": "top%20view.png",
+            "id": 1572110395,
+            "name": "Top View"
+        },
+        {
+            "icon": "front%20-%20dock.png",
+            "id": 1572110474,
+            "name": "Front - Dock"
+        }
+    ]
+};
