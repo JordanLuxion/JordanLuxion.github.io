8f08df0a71908825168ba43d664178c3768f27cc
JordanLuxion <69374962+JDoaneCSUF@users.noreply.github.com>
2020-08-12 13:37:20 -0700

    Shoe
    WM_Shoe

---
+++ ShoeConfig/keyshot-configurator-imagemapping.js
@@ -0,0 +1,81 @@
+imagemappingdata =
+[
+    {
+        "image": "1.26.1.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+17",
+            "22+23",
+            "26+27",
+            "29+31"
+        ],
+        "studio": 6294
+    },
+    {
+        "image": "1.26.2.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+18",
+            "22+24",
+            "26+28",
+            "29+30"
+        ],
+        "studio": 6294
+    },
+    {
+        "image": "1.26.3.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+17",
+            "22+23",
+            "26+27",
+            "29+31"
+        ],
+        "studio": 6295
+    },
+    {
+        "image": "1.26.4.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+18",
+            "22+24",
+            "26+28",
+            "29+30"
+        ],
+        "studio": 6295
+    },
+    {
+        "image": "1.26.5.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+17",
+            "22+23",
+            "26+27",
+            "29+31"
+        ],
+        "studio": 6293
+    },
+    {
+        "image": "1.26.6.png",
+        "modelsets": [
+            865176553
+        ],
+        "multimaterials": [
+            "14+18",
+            "22+24",
+            "26+28",
+            "29+30"
+        ],
+        "studio": 6293
+    }
+];
---
+++ ShoeConfig/keyshot-configurator-modeldata.js
@@ -0,0 +1,60 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "grey%20%2f%20black.png",
+                    "multimaterials": [
+                        "26+27",
+                        "14+17",
+                        "29+31",
+                        "22+23"
+                    ],
+                    "name": "Grey / Black"
+                },
+                {
+                    "icon": "blue%20%2f%20white.png",
+                    "multimaterials": [
+                        "26+28",
+                        "14+18",
+                        "29+30",
+                        "22+24"
+                    ],
+                    "name": "Blue / White"
+                }
+            ],
+            "name": "Color Combo 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Color Combo 1"
+            ],
+            "modelset": 865176553,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "left.png",
+            "id": 6293,
+            "name": "Left"
+        },
+        {
+            "icon": "bottom.png",
+            "id": 6294,
+            "name": "Bottom"
+        },
+        {
+            "checked": true,
+            "icon": "front%203%2f4.png",
+            "id": 6295,
+            "name": "Front 3/4"
+        }
+    ]
+};
